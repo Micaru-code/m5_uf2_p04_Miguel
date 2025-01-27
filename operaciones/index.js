ff0866fd5b498bb99af075d96ae238da0a5cd866
Miguel <micaru62@gmail.com>
2025-01-27 19:40:28 +0100

    modul de multiplicacion

--- operaciones/index.js
+++ operaciones/index.js
@@ -11,12 +11,16 @@ function suma(...valors) {
 
 /**
  * Aquesta funcio multiplica tots els valors passats per parametre
- * 
+ *
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
  */
 function multiplica(...valors) {
-
+    let resultat = 1;
+    for (let valor of valors) {
+        resultat *= valor;
+    }
+    return resultat;
 }
 
 module.exports.suma = suma;
